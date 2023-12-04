8d57a1a506cd6d66ed7327d463f9ad7ecb7ef375
Exxin <exxinblood@gmail.com>
2023-12-04 13:23:19 +0200

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,13 @@
 function getElementWidth(content, padding, border) {
+  var contentWidth = parseFloat(content);
+  var paddingWidth = parseFloat(padding);
+  var borderWidth = parseFloat(border);
 
-const contentWidth = parseFloat(content);
-const paddingWidth = parseFloat(padding);
-const borderWidth = parseFloat(border);
-    
-const totalWidth = contentWidth + paddingWidth + borderWidth;
+  var totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
 
-return totalWidth;
+  return totalWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
