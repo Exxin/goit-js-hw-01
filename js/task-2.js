256db19f2bf185871bc7d42d15f6b4049d5185e9
Exxin <exxinblood@gmail.com>
2023-12-04 13:20:31 +0200

    ф

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,14 @@
 function getShippingMessage(country, price, deliveryFee) {
-const totalPrice = `Shipping to ${country} will cost ${totalPrice} credits`
+  var totalPrice = price + deliveryFee;
+
+  var message = "Shipping to " + country + " will cost " + totalPrice + " credits";
+
+  return message;
 }
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20));
+var message1 = getShippingMessage("Australia", 120, 50);
+var message2 = getShippingMessage("Germany", 80, 20);
+var message3 = getShippingMessage("Sweden", 100, 20);
+
+console.log(message1);
+console.log(message2);
+console.log(message3);
